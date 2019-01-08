db3b2ecedc8fbec984552cfa5f88b33295300e59
ucrystal <sjsj7226@gmail.com>
2019-01-08 16:10:59 +0900

    import the logo and edit theme css

--- conf.py
+++ conf.py
@@ -19,8 +19,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'metatron-doc-user'
-copyright = '2018, metatron team'
+project = 'metatron discovery docs'
+copyright = '2019, metatron team'
 author = 'metatron team'
 
 # The short X.Y version
@@ -31,12 +31,15 @@ release = ''
 
 # -- General configuration ---------------------------------------------------
 
+def setup(app):
+    app.add_stylesheet('css/layout.css')
+
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
 
 # site base url
-site_url = 'https://9bow.github.io/metatron-doc-user'
+site_url = 'https://metatron.app/docs'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -86,7 +89,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -103,11 +108,15 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
+html_logo = "./_static/img/metatron-docs-logo.png"
+
+
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'metatron-doc-userdoc'
+#htmlhelp_basename = 'metatron-doc-userdoc'
+htmlhelp_basename = 'metatron-discovery-doc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -180,6 +189,7 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
 
 
 # -- Extension configuration -------------------------------------------------
