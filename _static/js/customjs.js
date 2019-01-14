c3a3a22c522e86b16ffd9075b3a4b947f15515ba
ucrystal <sjsj7226@gmail.com>
2019-01-14 20:10:49 +0900

    add custom js for switcing url by browser language

--- _static/js/customjs.js
+++ _static/js/customjs.js
@@ -1,5 +1,6 @@
-//var userLang = navigator.language || navigator.userLanguage; 
-//if(userLang == "ko") {window.location.replace('https://metatron-app.github.io/metatron-doc-discovery/kor/index.html');}
-//else{
-//	window.location.replace('https://metatron-app.github.io/metatron-doc-discovery/eng/index.html');
-//}
+var userLang = navigator.language || navigator.userLanguage; 
+if(userLang == "ko"){ 
+	document.location.replace('https://metatron-app.github.io/metatron-doc-discovery/kor/index.html');
+} else{
+	document.location.replace('https://metatron-app.github.io/metatron-doc-discovery/eng/index.html');
+}
