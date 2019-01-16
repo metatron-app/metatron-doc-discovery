6d9dbfa42e630ba797d9202462293a14f5d0c5f5
ucrystal <sjsj7226@gmail.com>
2019-01-16 10:23:15 +0900

    edit kor/en switching js

--- _static/js/customjs.js
+++ _static/js/customjs.js
@@ -1,6 +1,6 @@
 var userLang = navigator.language || navigator.userLanguage; 
 if(userLang == "ko"){ 
-	document.location.replace('./kor/index.html');
+	document.location.replace('../kor/index.html');
 } else{
-	document.location.replace('./eng/index.html');
+	document.location.replace('../eng/index.html');
 }
