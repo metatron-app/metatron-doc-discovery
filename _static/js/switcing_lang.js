b2235894285e159f1eeb762ed9b6165f050f8d3a
ucrystal <sjsj7226@gmail.com>
2019-01-24 09:59:17 +0900

    delete unused js

--- _static/js/switcing_lang.js
+++
@@ -1,6 +0,0 @@
-var userLang = navigator.language || navigator.userLanguage; 
-if(userLang == "ko"){ 
-	document.location.replace('../kor/index.html');
-} else{
-	document.location.replace('../eng/index.html');
-}
