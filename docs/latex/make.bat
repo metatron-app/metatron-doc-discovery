4b3006b12a8623771ec29f064e21e612c971ddc6
ucrystal <sjsj7226@gmail.com>
2019-11-22 17:16:57 +0900

    build guide docs

--- docs/latex/make.bat
+++ docs/latex/make.bat
@@ -8,7 +8,7 @@ set PDFLATEX=latexmk -pdf -dvi- -ps-
 
 set "LATEXOPTS= "
 
-set XINDYOPTS=-L general -C utf8  -M sphinx.xdy
+set XINDYOPTS=-L english -C utf8  -M sphinx.xdy
 set XINDYOPTS=%XINDYOPTS% -I xelatex
 if "%1" == "" goto all-pdf
 
